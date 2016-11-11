c9a77940ba3453a6b565c765157c25b35270360d
26115158 <26115158@stn.nitech.ac.jp>
2016-11-11 16:59:36 +0900

    課題5の発展課題

---
+++ kadai5/Frame/src/AIClassFrame.java
@@ -0,0 +1,15 @@
+/*
+ AIClassFrame.java
+ */
+
+/**
+ * クラスフレーム
+ */
+public class AIClassFrame extends AIFrame {
+
+	public AIClassFrame(AIFrameSystem inFrameSystem, AIClassFrame inSuperFrame,
+			String inName) {
+		super(inFrameSystem, inSuperFrame, inName, false);
+	}
+
+}
---
+++ kadai5/Frame/src/AIWhenConstructedProc.java
@@ -0,0 +1,13 @@
+/*
+ AIWhenConstructedProc.java
+ */
+
+public abstract class AIWhenConstructedProc {
+
+	public boolean isWhenConstructedProc() {
+		return true;
+	}
+
+	abstract public void eval(AIFrameSystem inFrameSystem, AIFrame inFrame);
+
+}
